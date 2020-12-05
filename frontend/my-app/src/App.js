80c8609abb45417fc4e84c7872c63c73489d984c
niklasu <niklasu@users.noreply.github.com>
2020-12-05 11:39:29 +0100

    cleanup

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -4,8 +4,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AppointmentOverview from "./Main";
 import Profile from "./Profile";
 
-const Example = () => <p>Example Komponente</p>;
-
 export default function App() {
     return (
         <Router>
